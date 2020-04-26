2a64db8094a761862115657782eca34115c9a340
AllynH <AllynH+github@gmail.com>
2020-04-26 20:57:01 +0100

    Removing old run script.

--- startup.bat
+++
@@ -1,8 +0,0 @@
-
-set my_path=%cd%
-@echo %my_path%
-
-
-start "Flask server" CMD /k "cd %my_path%\venv\Scripts & activate & cd %my_path% & code . & cd %my_path% & flask run" 
-start "Destiny-Focus Workspace" CMD /k "cd %my_path%\venv\Scripts & activate & cd %my_path%" 
-start "NPM React workspace" CMD /k "cd %my_path%\venv\Scripts & activate & cd ../../ & npm run webpack-watch"
