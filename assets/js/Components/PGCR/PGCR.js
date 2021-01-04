f97b3b1eb331f1f2a439d39b01554eaa5f7ac81b
AllynH <AllynH+github@gmail.com>
2021-01-04 10:54:50 +0000

    Updated raid PGCRs.

--- assets/js/Components/PGCR/PGCR.js
+++ assets/js/Components/PGCR/PGCR.js
@@ -79,6 +79,79 @@ export function PgcrDetailsPvP({ pgcr }) {
   )
 }
 
+export function PgcrDetailsRaid({ pgcr }) {
+  // console.log('pgcr')
+  // console.log(pgcr)
+  const entriesList = pgcr.Response.entries
+
+  return (
+    <ul className='pgcr-ul'>
+      <li className={'pgcr-char-wrap'} key={'header'}>
+        <span className={'pgcr player-name-icon'}>
+          <span className={'pgcr player-icon'}></span>
+          GUARDIAN
+        </span>
+        <span className={'pgcr player-stats'}>
+          <span>STATUS</span>
+        </span>
+        <span className={'pgcr player-stats'}>
+          <span>KILLS</span>
+        </span>
+        <span className={'pgcr player-stats'}>
+          <span>DEATHS</span>
+        </span>
+        <span className={'pgcr player-stats'}>
+          <span>ASSISTS</span>
+        </span>
+        <span className={'pgcr player-stats'}>
+          <span>K/D R</span>
+        </span>
+      </li>
+
+      {entriesList.map((element, index) => {
+        const pgcr_standing = element.values.completed.basic.displayValue === 'Yes' ?  'Completed' :  'Incomplete'         
+        const pgcr_icon = element.player.destinyUserInfo.iconPath
+        const pgcr_userName = element.player.destinyUserInfo.displayName
+        const pgcr_kdr = element.values.killsDeathsRatio.basic.displayValue
+        const pgcr_kda = element.values.killsDeathsAssists.basic.displayValue
+        const pgcr_deaths = element.values.deaths.basic.displayValue
+        const pgcr_kills = element.values.kills.basic.displayValue
+        const pgcr_assists = element.values.assists.basic.displayValue
+        const pgcr_team = element.values.assists.basic.displayValue
+        const iconStyle = {
+          backgroundImage: `url(https://www.bungie.net${pgcr_icon})`,
+          maxHeight: 30,
+        }
+
+        return (
+          <li className={'pgcr-char-wrap'} key={index}>
+            <span className={'pgcr pgcr-player-stats player-name-icon'}>
+              <span className={'pgcr player-icon'} style={iconStyle}></span>
+              {pgcr_userName}
+            </span>
+            <span className={'pgcr player-stats'}>
+              <span>{pgcr_standing}</span>
+            </span>
+            <span className={'pgcr player-stats'}>
+              <span>{pgcr_kills}</span>
+            </span>
+            <span className={'pgcr player-stats'}>
+              <span>{pgcr_deaths}</span>
+            </span>
+            <span className={'pgcr player-stats'}>
+              <span>{pgcr_assists}</span>
+            </span>
+            <span className={'pgcr player-stats'}>
+              <span>{pgcr_kdr}</span>
+            </span>
+          </li>
+        )
+
+      })}
+    </ul>
+  )
+}
+
 export function PgcrDetailsGambit({ pgcr }) {
   console.log('PgcrDetailsGambit')
   console.log(pgcr)
@@ -217,6 +290,8 @@ export default function Pgcr(props) {
     switch (gameMode) {
       case 'gambit':
         return <PgcrDetailsGambit pgcr={pgcr} />
+      case 'raid':
+        return <PgcrDetailsRaid pgcr={pgcr} />
       default:
         return <PgcrDetailsPvP pgcr={pgcr} />
     }
