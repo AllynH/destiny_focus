59b3e70db4273b04aecfe919a1acc159422888d4
AllynH <AllynH+github@gmail.com>
2021-06-06 15:37:10 +0100

    Fixed multiple NPM modules and changed Welcome component to use SVG.

--- assets/js/Components/Welcome/Welcome.js
+++ assets/js/Components/Welcome/Welcome.js
@@ -1,21 +1,38 @@
 import React from 'react'
+
+import Crucible from '../../../img/icons/Crucible.svg'
+
 import './style.css'
 
 export default function Welcome() {
   return (
     <section className='welcome-content'>
-      <div className='welcome-wrapper'>
-        <div className='blur-wrap' >
-          <h1 className='welcome'>Welcome to Destiny Focus</h1>
-        </div>
-        <h2>
-          Focus on <span className='text_rotate'></span>
-        </h2>
+      <div className='welcome-content-image-wrapper'>
+        <div className='welcome-wrapper'>
+          <div className='blur-wrap'>
+            <h1 className='welcome'>Welcome to Destiny Focus</h1>
+          </div>
+          <h2>
+            Focus on <span className='text_rotate'></span>
+          </h2>
 
-        <p className='welcome-text'>Authorise your account with Bungie to continue.</p>
-        <a className='welcome-button' tabIndex="0" role="button" aria-pressed="false" href={'/authorize/bungie'}>
-          Authorise
-        </a>
+          <p className='welcome-text'>Authorise your account with Bungie to continue.</p>
+          <a
+            className='welcome-button'
+            tabIndex='0'
+            role='button'
+            aria-pressed='false'
+            href={'/authorize/bungie'}
+          >
+            Authorise
+          </a>
+        </div>
+        <div className='welcome-image-placement'>
+          <Crucible
+            className='welcome-image'
+            viewBox={'0 0 25 25'}
+          />
+        </div>
       </div>
     </section>
   )
